04ba21a1246cc13d457f0c4a04434f7f96b3d764
yan <yanajiki2010@hotmail.com>
2024-03-13 21:36:35 -0300

    feat: tela de criação de sala

--- app/create-game/page.tsx
+++ app/create-game/page.tsx
@@ -2,36 +2,45 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { FormEvent } from "react";
+import Link from "next/link";
+import { redirect, useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 export default function ProfileForm() {
-    async function onSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
-        console.log(event.currentTarget)
-        const formData = new FormData(event.currentTarget)
-        // const response = await fetch('/api/submit', {
-        //   method: 'POST',
-        //   body: formData,
-        // })
-     
-        // Handle response if necessary
-        // const data = await response.json()
-        // ...
-      }
+  const [roomName, setRoomName] = useState('');
+  const router = useRouter()
 
-    return (
-        <>
-          <div className="flex justify-between h-screen items-center">
-            <div className="flex flex-row m-auto p-4 shadow-lg border rounded bg-cyan-100">
-              <Input 
-                className="border-1 mr-4 hover:border-slate-700 shadow-sm focus:border-cyan-500"
-                placeholder="Nome da sala"
-              />
-              <Button className="bg-cyan-500">
-                Confirmar
-              </Button>
-            </div>
-          </div>
-        </>
-      );
+  async function createRoom() {
+    await fetch('https://dummyjson.com/products/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: roomName,
+        /* other product data */
+      })
+    })
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+        router.push('/')
+      });
+  }
+
+  return (
+    <>
+      <div className="flex justify-between h-screen items-center">
+        <div className="flex flex-row m-auto p-4 shadow-lg border rounded bg-cyan-100">
+          <Input
+            className="border-1 mr-4 hover:border-slate-700 shadow-sm focus:border-cyan-500"
+            placeholder="Nome da sala"
+            value={roomName}
+            onChange={e => setRoomName(e.target.value)}
+          />
+          <Button className="bg-cyan-500" onClick={createRoom}>
+            Confirmar
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
