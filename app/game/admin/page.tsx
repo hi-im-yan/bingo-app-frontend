56ec61e5d100b595904acf3777a64c359886a0f7
yan <yanajiki2010@hotmail.com>
2024-04-03 21:40:19 -0400

    feat: showing the numbers of corresponding letter

--- app/game/admin/page.tsx
+++ app/game/admin/page.tsx
@@ -1,36 +1,54 @@
-"use client"
-
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import { redirect, useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 
 export default function ProfileForm() {
-  const [roomName, setRoomName] = useState('');
-  const router = useRouter()
+  const [selectedButton, setSelectedButton] = useState("");
 
-  async function createRoom() {
-    // await fetch('https://dummyjson.com/products/add', {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({
-    //     title: roomName,
-    //     /* other product data */
-    //   })
-    // })
-    //   .then(res => res.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //     router.push('/')
-    //   });
-  }
+  const handleButtonClick = (letter: string) => {
+    setSelectedButton(letter);
+  };
+
+  const letterMappings: Record<string, number[]> = {
+    B: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    I: [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+    N: [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
+    G: [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+    O: [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75],
+  };
 
   return (
     <>
-    <h1>Game Admin page</h1>
+      <div className="border border-gray-300 shadow-lg p-4 lg:w-1/4 sm:w-max"> 
+        <div className="grid grid-cols-5">
+          {["B", "I", "N", "G", "O"].map((letter) => (
+            <Button
+              key={letter}
+              className={`bg-${
+                selectedButton.match(letter) ? "blue-700" : "blue-500"
+              } hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border border-gray-200 m-1`}
+              onClick={() => handleButtonClick(letter)}
+            >
+              {letter}
+            </Button>
+          ))}
+        </div>
+        <hr className="my-4" />
+        <div className="grid grid-cols-5">
+          {letterMappings[selectedButton]?.map((number, index) => (
+            <div key={number}>
+              <Button
+                className="bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded border border-gray-200 mb-1 hover:bg-gray-300"
+              >
+                {number.toFixed(0).padStart(2, "0")}
+              </Button>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
